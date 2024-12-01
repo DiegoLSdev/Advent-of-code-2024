d644b174924e605da4292be2f54fccb43e280a1e
DiegoLSdev <diegolajusticiasujas@icloud.com>
2024-12-01 20:36:29 +0100

    Day 1 Part 2

--- scripts/day1.py
+++ scripts/day1.py
@@ -35,5 +35,47 @@ def part1(input_data):
     return total_distances 
 
 def part2(input_data):
-    return ""
+
+    # Initialize variables for the left and right columns
+    left_col = []
+    right_col = []
+
+    # Split the input data by new lines to create pairs
+    pairs = input_data.split("\n")
+
+    # Iterate over each pair and split it into left and right values
+    for i in pairs:
+        right_col.append(i.split("   ")[1])
+        left_col.append(i.split("   ")[0])
+
+    # Turn each item of the column into a Integer
+    right_col_int = [int(x) for x in right_col] # Left column: first part of the pair
+    left_col_int = [int(x) for x in left_col] # Right column: second part of the pair
+
+    # Did not use Counter library so created check frequency function
+    def check_frequency(arr1, arr2):
+        result = {}
+        for num in arr1:
+            count = 0  
+            for item in arr2:
+                if item == num:
+                    count += 1  
+            result[num] = count 
+        return result
+
+    # Call Function
+    result = check_frequency(left_col_int, right_col_int)
+
+    # Initialize empty array to store the calculated frequencies
+    list_of_frequencies = []
+
+    # Calculate each frequency
+    for key, value in result.items():
+        if value > 0:
+            list_of_frequencies.append(key*value)
+    
+    # Sum all the distances to get the total
+    frequencies = sum(list_of_frequencies)
+
+    return frequencies
  
