b71300a66ffd9c4b72e00fc90da262c86ba1b406
DiegoLSdev <diegolajusticiasujas@icloud.com>
2024-12-04 00:01:52 +0100

    Day 3 Part 2

--- scripts/day3.py
+++ scripts/day3.py
@@ -19,6 +19,35 @@ def part1(input_data):
     return sum(multiplied_pairs) # Sum of pairs multiplied
 
 
-def part2(input_data):
+import re
 
-    return "Not done yet"
+def part2(input_data):
+    # Storage of pairs to multiply
+    multiplied_pairs = []
+    
+    # Patterns
+    pattern_do = r"do\(\)"
+    pattern_dont = r"don't\(\)"
+    pattern_mul = r"mul\((\d{1,3}),(\d{1,3})\)"
+    
+    # Divide input_data into chunks that starts with "do()" and ends with "don't()""
+    valid_sections = re.split(f"({pattern_do}|{pattern_dont})", input_data)
+    
+    # Variable for controlling if we are inside "do()" loop 
+    enabled = True
+    
+    # Loop over valid_sections
+    for section in valid_sections:
+        section = section.strip()
+
+        if section == "do()":
+            enabled = True  # Initial process turned on (code will go on with this section)
+        elif section == "don't()":
+            enabled = False  # Initial process turned off (code will not go on with this section)
+        elif enabled:  
+            # # Find & Store chunks matching pattern
+            correct_chunks = re.findall(pattern_mul, section)
+            for n1, n2 in correct_chunks:
+                multiplied_pairs.append(int(n1) * int(n2))
+    
+    return sum(multiplied_pairs) # Sum of pairs multiplied
